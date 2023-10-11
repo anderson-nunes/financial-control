198fe7f097738a30f99443c60cca6d2c064714b4
anderson-nunes <andersonribeironunes@hotmail.com>
2023-10-11 18:30:22 -0300

    Criado api para o json server

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5757",
+  baseURL: "https://fake-api-nyxz.onrender.com",
 });
 
 // "https://json-teste-vh2v-git-main-anderson-nunes.vercel.app/";
